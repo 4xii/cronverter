88e92f98197f9af8cd47db8bd9aec6a8aaead5d9
4xii <975795875@qq.com>
2024-07-03 17:00:50 +0800

    style: Refactor code for consistent styling and semicolon removal

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,42 @@
 type CrontabEntry = {
-  schedule: string;
-  task: string;
-};
-
-class CrontabConverter {
-  static crontabToJson(crontab: string): CrontabEntry[] {
-    return crontab.split('\n').filter(line => line.trim() !== '').map(line => {
-      const parts = line.split(' ');
-      if (parts.length < 6) {
-        throw new Error('Invalid crontab format: each line must have at least 6 parts');
-      }
-      const schedule = parts.slice(0, 5).join(' ');
-      const task = parts.slice(5).join(' ');
-      return { schedule, task };
-    });
-  }
-
-  static jsonToCrontab(json: CrontabEntry[]): string {
-    if (!Array.isArray(json)) {
-      throw new Error('Invalid JSON format: input must be an array');
-    }
-    return json.map(entry => {
-      if (typeof entry !== 'object' || !entry.schedule || !entry.task) {
-        throw new Error('Invalid JSON format: each entry must be an object with schedule and task properties');
-      }
-
-      return `${entry.schedule} ${entry.task}`;
-    }).join('\n');
-  }
+  schedule: string
+  task: string
 }
 
-export default CrontabConverter;
+const CrontabConverter = {
+  crontabToJson(crontab: string): CrontabEntry[] {
+    return crontab
+      .split('\n')
+      .filter((line) => line.trim() !== '')
+      .map((line) => {
+        const parts = line.split(' ')
+        if (parts.length < 6) {
+          throw new Error(
+            'Invalid crontab format: each line must have at least 6 parts',
+          )
+        }
+        const schedule = parts.slice(0, 5).join(' ')
+        const task = parts.slice(5).join(' ')
+        return { schedule, task }
+      })
+  },
 
+  jsonToCrontab(json: CrontabEntry[]): string {
+    if (!Array.isArray(json)) {
+      throw new TypeError('Invalid JSON format: input must be an array')
+    }
+    return json
+      .map((entry) => {
+        if (typeof entry !== 'object' || !entry.schedule || !entry.task) {
+          throw new Error(
+            'Invalid JSON format: each entry must be an object with schedule and task properties',
+          )
+        }
+
+        return `${entry.schedule} ${entry.task}`
+      })
+      .join('\n')
+  },
+}
+
+export default CrontabConverter
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,62 +1,61 @@
-import { describe, it, expect } from 'vitest';
-import CrontabConverter from '../src';
+import { describe, expect, it } from 'vitest'
+import CrontabConverter from '../src'
 
 describe('CrontabConverter', () => {
   it('should convert crontab to JSON', () => {
     const crontab = `0 5 * * * /path/to/command1
 30 6 * * * /path/to/command2
-15 14 1 * * /path/to/command3`;
+15 14 1 * * /path/to/command3`
 
     const expectedJson = [
       { schedule: '0 5 * * *', task: '/path/to/command1' },
       { schedule: '30 6 * * *', task: '/path/to/command2' },
       { schedule: '15 14 1 * *', task: '/path/to/command3' },
-    ];
+    ]
 
-    const json = CrontabConverter.crontabToJson(crontab);
-    expect(json).toEqual(expectedJson);
-  });
+    const json = CrontabConverter.crontabToJson(crontab)
+    expect(json).toEqual(expectedJson)
+  })
 
   it('should convert JSON to crontab', () => {
     const json = [
       { schedule: '0 5 * * *', task: '/path/to/command1' },
       { schedule: '30 6 * * *', task: '/path/to/command2' },
       { schedule: '15 14 1 * *', task: '/path/to/command3' },
-    ];
+    ]
 
-    const expectedCrontab =
-      `0 5 * * * /path/to/command1
+    const expectedCrontab = `0 5 * * * /path/to/command1
 30 6 * * * /path/to/command2
-15 14 1 * * /path/to/command3`;
+15 14 1 * * /path/to/command3`
 
-    const crontab = CrontabConverter.jsonToCrontab(json);
-    expect(crontab).toBe(expectedCrontab);
-  });
-
+    const crontab = CrontabConverter.jsonToCrontab(json)
+    expect(crontab).toBe(expectedCrontab)
+  })
 
   it('should throw an error if crontab format is invalid', () => {
-    const invalidCrontab = `0 5 * * *`;
+    const invalidCrontab = `0 5 * * *`
 
     expect(() => {
-      CrontabConverter.crontabToJson(invalidCrontab);
-    }).toThrow('Invalid crontab format: each line must have at least 6 parts');
-  });
+      CrontabConverter.crontabToJson(invalidCrontab)
+    }).toThrow('Invalid crontab format: each line must have at least 6 parts')
+  })
 
   it('should throw an error if JSON format is invalid (not an array)', () => {
-    const invalidJson = {};
+    const invalidJson = {}
 
     expect(() => {
-      CrontabConverter.jsonToCrontab(invalidJson as any);
-    }).toThrow('Invalid JSON format: input must be an array');
-  });
+      CrontabConverter.jsonToCrontab(invalidJson as any)
+    }).toThrow('Invalid JSON format: input must be an array')
+  })
 
   it('should throw an error if JSON format is invalid (missing properties)', () => {
-    const invalidJson = [{ schedule: '0 5 * * *' }];
+    const invalidJson = [{ schedule: '0 5 * * *' }]
 
     expect(() => {
       // @ts-expect-error
-      CrontabConverter.jsonToCrontab(invalidJson);
-    }).toThrow('Invalid JSON format: each entry must be an object with schedule and task properties');
-  });
-});
-
+      CrontabConverter.jsonToCrontab(invalidJson)
+    }).toThrow(
+      'Invalid JSON format: each entry must be an object with schedule and task properties',
+    )
+  })
+})
